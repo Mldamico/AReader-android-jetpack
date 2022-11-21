c3ce3525fbd5fc6d3f1e83607ede7906b1fb5306
mldamico <matias.lioneldamico@gmail.com>
2022-11-21 16:49:50 -0300

    Basic setup

---
+++ app/src/main/java/com/example/areader/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.areader
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.areader.ui.theme.AReaderTheme
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AReaderTheme {
+
+                Surface(color = MaterialTheme.colors.background) {
+
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AReaderTheme {
+        Greeting("Android")
+    }
+}
