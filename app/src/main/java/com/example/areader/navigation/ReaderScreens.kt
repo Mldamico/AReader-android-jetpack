457476d8d93411fcc53339424c23d66c587f4eed
mldamico <matias.lioneldamico@gmail.com>
2022-11-21 17:46:31 -0300

    navigation

--- app/src/main/java/com/example/areader/navigation/ReaderScreens.kt
+++ app/src/main/java/com/example/areader/navigation/ReaderScreens.kt
@@ -1,4 +1,26 @@
 package com.example.areader.navigation
 
 enum class ReaderScreens {
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    HomeScreen,
+    SearchScreen,
+    DetailsScreen,
+    UpdateScreen,
+    StatsScreen;
+    companion object {
+        fun fromRoute(route: String?): ReaderScreens= when(route?.substringBefore('/')){
+            SplashScreen.name -> SplashScreen
+            LoginScreen.name -> LoginScreen
+            CreateAccountScreen.name -> CreateAccountScreen
+            SearchScreen.name -> SearchScreen
+            HomeScreen.name -> HomeScreen
+            DetailsScreen.name -> DetailsScreen
+            UpdateScreen.name -> UpdateScreen
+            StatsScreen.name -> StatsScreen
+            null -> HomeScreen
+            else -> throw java.lang.IllegalArgumentException("Route $route is not recognized")
+        }
+    }
 }
